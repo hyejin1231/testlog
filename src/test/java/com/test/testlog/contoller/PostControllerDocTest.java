1c97e8c8fdecdebfd68dde704aee57d73a8ea724
hyejin <phj981231@naver.com>
2024-02-10 11:18:33 +0900

    [2024.02.10] 섹션2. 문서화 - Spring Rest Docs1 : 요청, 응답 필드

--- src/test/java/com/test/testlog/contoller/PostControllerDocTest.java
+++ src/test/java/com/test/testlog/contoller/PostControllerDocTest.java
@@ -1,44 +1,51 @@
 package com.test.testlog.contoller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.testlog.domain.Post;
 import com.test.testlog.repository.PostRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.test.testlog.request.PostCreate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.hypermedia.HypermediaDocumentation;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(
+        uriScheme = "https", uriHost = "api.testlog.com", uriPort = 443
+)
 @ExtendWith(RestDocumentationExtension.class)
 public class PostControllerDocTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private PostRepository postRepository;
 
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        // 자동주입이 아니라 테스트 전에 미리 mockMvc 세팅하는 작업
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
+    @Autowired
+    private ObjectMapper objectMapper;
+
+//    @BeforeEach
+//    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+//        // 자동주입이 아니라 테스트 전에 미리 mockMvc 세팅하는 작업
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//                .apply(documentationConfiguration(restDocumentation))
+//                .build();
+//    }
 
     @Test
     @DisplayName("글 단건 조회 테스트")
@@ -50,11 +57,45 @@ public class PostControllerDocTest {
                 .build();
         postRepository.save(post);
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/posts/{postId}", 1L)
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{postId}", 1L)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk())
-                .andDo(MockMvcRestDocumentation.document("index"));
+                .andDo(MockMvcRestDocumentation.document("index",
+                        RequestDocumentation.pathParameters(
+                                RequestDocumentation.parameterWithName("postId").description("게시글 ID")
+                        ),
+                        PayloadDocumentation.responseFields(
+                            PayloadDocumentation.fieldWithPath("id").description("게시글 ID"),
+                                PayloadDocumentation.fieldWithPath("title").description("게시글 제목"),
+                                PayloadDocumentation.fieldWithPath("content").description("게시글 내용")
+
+                        )
+
+                ));
+    }
+
+    @Test
+    @DisplayName("글 등록 테스트")
+    void testRestDocs2() throws Exception {
+        // given
+        PostCreate request = PostCreate.builder()
+                .title("test log title")
+                .content("test log contnet")
+                .build();
+
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/posts")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andDo(MockMvcRestDocumentation.document("index",
+                        PayloadDocumentation.requestFields(
+                                PayloadDocumentation.fieldWithPath("title").description("게시글 제목"),
+                                PayloadDocumentation.fieldWithPath("content").description("게시글 내용")
+                        )
+                ));
     }
 
 }
