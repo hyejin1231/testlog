04dd95f5d20123aa2c87af64991b7b065acc876a
hyejin <phj981231@naver.com>
2024-02-12 15:15:22 +0900

    [2024.02.12] 섹션4. Vue.js3 - CORS 문제 해결하기 (프론트엔드에서)

--- src/main/java/com/test/testlog/config/WebConfig.java
+++ src/main/java/com/test/testlog/config/WebConfig.java
@@ -7,9 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer
 {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5173/");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://127.0.0.1:5173/");
+//    }
 }
